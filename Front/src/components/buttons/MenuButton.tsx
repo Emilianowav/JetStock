657f3579861413fcb687c4fa9e5cfad6aa7422a8
Emiliano <emidbiasi@gmail.com>
2025-02-07 19:25:44 -0300

    fixing production

--- Front/src/components/buttons/MenuButton.tsx
+++ Front/src/components/buttons/MenuButton.tsx
@@ -5,7 +5,7 @@ import styles from "./MenuButton.module.css";
 interface ButtonProps {
   text: string;
   onClick: () => void;
-  icon: React.ReactNode; // Acepta un icono
+  // icon: React.ReactNode; 
   type?: "button" | "submit" | "reset";
   className?: string;
   isActive?: boolean; // Nueva prop para indicar si el botón está activo
@@ -14,7 +14,6 @@ interface ButtonProps {
 const MenuButton: React.FC<ButtonProps> = ({
   text,
   onClick,
-  icon,
   type = "button",
   className = "",
   isActive = false,
@@ -25,7 +24,7 @@ const MenuButton: React.FC<ButtonProps> = ({
       className={`${styles.button} ${isActive ? styles.active : ""} ${className}`}
       onClick={onClick}
     >
-      <span className={styles.icon}>{icon}</span> {/* Muestra el icono */}
+      {/* <span className={styles.icon}>{icon}</span> */}
       {text}
     </button>
   );
